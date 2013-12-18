a5efdfeb40708b63487ca5f3d559322505fa39f2
Robert <201292@live.nl>
2013-12-18 14:29:13 +0100

    added functionality to library.

--- DmDisplay.cpp
+++ DmDisplay.cpp
@@ -4,20 +4,16 @@
 //like setting data port to output.
 //and setting the control lines to output.
 //and call init to init the display.
-DmDisplay::DmDisplay()
+DmDisplay::DmDisplay(void)
 {
 	FUNCTIONPORT_TO_OUTPUT;
 	DATAPORT_TO_OUTPUT;
-	
-	CLEAR_POWER;
-	_delay_ms(100);
-	SET_POWER;
 	_delay_ms(100);
 	init();
 	_delay_ms(100);
 }
 //initializes the display.
-void DmDisplay::init()
+void DmDisplay::init(void)
 {
 	write(0xE2, INSTRUCT);
 	_delay_ms(1000);
@@ -114,7 +110,7 @@ void DmDisplay::setMarker(uint8_t marker, bool state)
 //inverts display turns pixels that are on off, and off to on.
 void DmDisplay::invertDisplay(bool reverse)
 {
-	write(0xA6+(!reverse), INSTRUCT);
+	write(0xA6+(reverse), INSTRUCT);
 }
 //sets column address, thus which column of bytes to address. (bytes represents pixels.)
 void DmDisplay::setCol(uint8_t col)
@@ -143,8 +139,11 @@ void DmDisplay::lcdChar(const char *str)
 {
 	int charCount = strlen(str);
 	
+	//int charCount = 0;
+	//while(str[charCount] != '\0') charCount++;
+	
 	//if there are more chars then 
-	//there is room on a line.
+	//there is room on a line. just make it line size.
 	if(charCount > 20)
 	{
 		charCount = 20;
@@ -152,6 +151,7 @@ void DmDisplay::lcdChar(const char *str)
 	
 	//start read-modify-write
 	write(0xE0, INSTRUCT);
+	//a loop that write the characters to the screen.
 	for(int i = 0;i<charCount;i++)
 	{
 		uint8_t currentChar = str[i];
@@ -167,22 +167,36 @@ void DmDisplay::lcdChar(const char *str)
 	resetColumnAdress();
 }
 //fill screen with spaces.
-//so it seems cleared.
-void DmDisplay::clear()
+//so it seems cleared. well is cleared.
+void DmDisplay::clear(void)
 {
+	home();
 	for(int i = 0;i<6;i++)
 	{
 		setRow(i);
 		lcdChar("                    ");
 	}
 }
-//set write location for for example writing pixel data.
-void DmDisplay::setWriteAddress(uint8_t columnAddr, uint8_t page)
+//set write location for example writing pixel data.
+//single pixel distances.
+void DmDisplay::setWriteReadAddres(uint8_t columnAddr, uint8_t page)
 {
-	setRow(page);
+	//bound check
+	if(page >= 6)
+			page = 6;
+	if(columnAddr >= 100)
+			columnAddr = 100;
+	if(page < 0)
+		page = 0;
+	if(columnAddr < 0)
+		columnAddr = 0;
+	//set columnAddr.
 	setCol(columnAddr);
+	//set page adress.
+	setRow(page);
 }
 //set cursor to location x,y with in bounds of the screen size.
+//uses character distances.
 void DmDisplay::setCursor(uint8_t x, uint8_t y)
 {
 	// this function wil do nothing if you specify numbers
@@ -192,24 +206,30 @@ void DmDisplay::setCursor(uint8_t x, uint8_t y)
 		if(y < 6)
 		{
 			//font width is 5
-			setWriteAddress(x*5, y);
+			setWriteReadAddres(x*5, y);
 		}	
 	}
 }
 //sets print location to 0.0
-void DmDisplay::home()
+//uses character distances.
+void DmDisplay::home(void)
 {
 	setCursor(0,0);
 }
 //resets column addres aka sets to position 0,x
-void DmDisplay::resetColumnAdress()
+//single pixel distances
+void DmDisplay::resetColumnAdress(void)
 {
-	write(0x10, INSTRUCT);
-	write(0x00, INSTRUCT);
+	setCol(0);
 }
-
+//resets row addres. aka sets to position x,0
+//single pixel distances.
+void DmDisplay::resetRowAdress(void)
+{
+	setRow(0);
+}
 //for testing purposes
-void DmDisplay::blinkControlLines()
+void DmDisplay::blinkControlLines(void)
 {
 	SET_A0;
 	SET_ENABLE;
@@ -220,3 +240,179 @@ void DmDisplay::blinkControlLines()
 	CLEAR_RW;
 	_delay_ms(250);
 }
+
+
+//draws a line.
+//based on this line drawing function:
+//https://code.google.com/p/glcd-arduino/downloads/detail?name=glcd-v3-20111205.zip&can=2&q=
+//which source says its actually based on:
+//http://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+void DmDisplay::drawLine(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2)
+{
+	int deltax, deltay, x,y, steep;
+	int error, ystep;
+	
+	steep = abs(y1, y2) > abs(x1 ,x2);  
+
+	if (steep)
+	{
+		swap(x1, y1);
+		swap(x2, y2);
+	}
+
+	if (x1 > x2)
+	{
+		swap(x1, x2);
+		swap(y1, y2);
+	}
+
+	deltax = x2 - x1;
+	deltay = abs(y2, y1);  
+	error = deltax / 2;
+	y = y1;
+	if(y1 < y2) ystep = 1;  else ystep = -1;
+
+	for(x = x1; x <= x2; x++)
+	{
+		if (steep) writePixel(y,x); else writePixel(x,y);
+   		error = error - deltay;
+		if (error < 0)
+		{
+			y = y + ystep;
+			error = error + deltax;
+    	}
+	}
+}
+//drawing a rectangle
+void DmDisplay::drawRect(uint8_t x, uint8_t y, uint8_t width, uint8_t height)
+{
+	//upper x line.
+	drawLine(x,y, x+width, y);
+	//lower x line.
+	drawLine(x, y+height, x+width, y+height);
+	//left y line.
+	drawLine(x,y, x, y+height);
+	//right y line.
+	drawLine(x+width,y, x+width, y+height);
+}
+//implementation of bresenham's circle
+void DmDisplay::drawCircle(uint8_t x0, uint8_t y0, uint8_t radius)
+{
+	int error = 1 - radius;
+	int errorY = 1;
+	int errorX = -2 * radius;
+	int x = radius, y = 0;
+	
+	writePixel(x0, y0 + radius);
+	writePixel(x0, y0 - radius);
+	writePixel(x0 + radius, y0);
+	writePixel(x0 - radius, y0);
+	
+	while(y< x)
+	{
+		if(error > 0)
+		{
+			x--;
+			errorX += 2;
+			error += errorX;
+		}
+		y++;
+		errorY += 2;
+		error += errorY;
+		
+		writePixel(x0 + x, y0 + y);
+		writePixel(x0 - x, y0 + y);
+		writePixel(x0 + x, y0 - y);
+		writePixel(x0 - x, y0 - y);
+		writePixel(x0 + y, y0 + x);
+		writePixel(x0 - y, y0 + x);
+		writePixel(x0 + y, y0 - x);
+		writePixel(x0 - y, y0 - x);
+	}
+}
+//this thing fails big time.
+void DmDisplay::drawArc(float x, float y, int r, float start_angle, float end_angle)
+{
+	// I know the question is tagged c++, but the idea comes clear in javascript
+	
+	float step = 0.1;
+	for(float i = start_angle; i < end_angle; i = i + step)
+	{
+		float x0 = x+cos(i)*r; 
+		float y0 = y+sin(i)*r;
+		float x1 = x+cos(i+step)*r;
+		float y1 = y+sin(i+step)*r;
+		//writePixel(30 + cos(i) * r, 20 + sin(i) * r); // center point is (50,100)
+		drawLine(x0, y0, x1, y1);
+	}
+}
+
+void DmDisplay::clearMarkers(void)
+{
+	for(int i = 0;i<7;i++)
+		setMarker(i, 0);
+}
+
+//reads pixel data, on location set by setWriteReadAdress.
+uint8_t DmDisplay::read()
+{
+	uint8_t data = 0;
+	
+	//enter read modify write.
+	write(0xE0, INSTRUCT);
+	
+	//set port to input.
+	DATAPORT_TO_INPUT;
+	//clear input port to be sure.
+	DATA_INPUT = 0;
+	//set apropriate controlle lines. 
+	SET_RW;
+	SET_A0;
+	//toggle enable twice. dummy read+actuall read.
+	toggleEnable();
+	_delay_us(10);
+	toggleEnable();
+	_delay_us(10);
+	//read in the data.
+	data = DATA_INPUT;
+	//set the port back to output.
+	DATAPORT_TO_OUTPUT;
+	//write back data we read (datasheet dummy-read -> read -> write. (read modify write)).
+	write(data, DATA);
+	//leave read modify write.
+	write(0xEE, INSTRUCT);
+	return data;
+}
+
+//write a pixel.
+void DmDisplay::writePixel(uint8_t x, uint8_t y)
+{
+	//for holding existing pixel data.
+	uint8_t pixelData = 0;
+	
+	if((x >= 100)||(y >= 48))
+		return;
+	if((x < 0) || (y < 0))
+		return;
+	
+	//set read/write location.
+	//also read pixel data in so we can OR it with the already existing data.
+	setWriteReadAddres(x, y/8);
+	pixelData = read();
+	
+	//determine what pixel in the pixel column to set.
+	uint8_t pixelByteCol = (1<<(y%8));
+	//write data and or it with that was already there.
+	write(pixelByteCol|pixelData, DATA);
+}
+
+//toggles Enable
+void DmDisplay::toggleEnable(void)
+{
+	SET_ENABLE;
+	_delay_us(10);
+	CLEAR_ENABLE;
+	_delay_us(10);
+	SET_ENABLE;
+}
+
--- DmDisplay.h
+++ DmDisplay.h
@@ -7,12 +7,15 @@
 #include "font5x7.h"
 #include "pinDefs.h"
 
+#define abs(x, y) 		((x>y) ?  (x-y) : (y-x))
+#define swap(a, b) 		do{a = a+b;b = a - b;a = a-b;}while(0);
+
 class DmDisplay
 {
 	public:
-		DmDisplay();
-		void init();
-		void blinkControlLines();
+		DmDisplay(void);
+		void init(void);
+		void blinkControlLines(void);
 		void write(uint8_t, uint8_t);
 		void write(uint8_t);
 		void setContrast(uint8_t);
@@ -20,13 +23,24 @@ class DmDisplay
 		void invertDisplay(bool);
 		void setRow(uint8_t);
 		void lcdChar(const char *);
-		void resetColumnAdress();
+		void resetColumnAdress(void);
+		void resetRowAdress(void);
 		void setCol(uint8_t col);
 		void toggleDisplayOnOff(bool);
-		void setWriteAddress(uint8_t columnAddr, uint8_t page);
+		void setWriteReadAddres(uint8_t columnAddr, uint8_t page);
 		void setCursor(uint8_t, uint8_t);
-		void clear();
-		void home();
+		void clear(void);
+		void home(void);
+		void clearMarkers(void);
+		void toggleEnable(void);
+		void writePixel(uint8_t, uint8_t);
+		uint8_t read();
+		void drawArc(float, float, int, float, float);
+		void drawCircle(uint8_t, uint8_t, uint8_t);
+		void drawRect(uint8_t, uint8_t, uint8_t, uint8_t);
+		void drawLine(uint8_t, uint8_t, uint8_t, uint8_t);
+		
+		
 		enum
 		{
 			INSTRUCT,DATA
